aa03fe8e5e30278ed2df588d2152785233f3380a
Alex Tzimas <tzalex@outlook.com.gr>
2021-07-27 15:29:30 +0300

    Create template to display cells on screen
    This is currently working but it is only
    an empty grid.

--- src/main/scala/Life.scala
+++ src/main/scala/Life.scala
@@ -1,5 +1,28 @@
 object Life {
     def main(args: Array[String]): Unit = {
         println("Welcome to Conway's Game of Life!")
+        Display show
   }
+}
+
+object Display {
+    private val grid_rows = 10
+    private val grid_columns = 10
+    var grid = (
+        for (i <- 1 to grid_rows) yield {
+            for (j <- 1 to grid_columns) yield "o"
+        }.toArray
+    ).toArray
+
+    def show: Unit = {
+        println(grid_formatted)
+    }
+
+    def grid_formatted: String = {
+        val border: String  = s"+${"-" * (grid_columns * 2 - 1)}+\n" // e.g. +-----------+
+        val rows_template: String = ("|%s " + "%s " * (grid_columns - 2) + "%s|\n") 
+        val grid_bordered: Array[String] = grid.map(x => rows_template.format(x: _*))
+        border + grid_bordered.foldLeft("")(_ + _) + border
+    }
+    
 }
